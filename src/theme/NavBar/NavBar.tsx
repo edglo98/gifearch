5c9c9b4afe589583bda1d1adbc7fb8662d808d96
Edgar Lopez <edgarlopezmgs@live.com>
2022-01-08 18:56:57 -0600

    styles: appbar width delimiter

--- src/theme/NavBar/NavBar.tsx
+++ src/theme/NavBar/NavBar.tsx
@@ -4,24 +4,26 @@ import styles from './NavBar.module.css'
 export const NavBar = () => {
   return (
     <header className={styles.appbar} >
-      <section className={styles.logo}>
-        <Link to='/'>
-          Gifearch
-        </Link>
+      <section className={styles.appcontainer} >
+        <section className={styles.logo}>
+          <Link to='/'>
+            Gifearch
+          </Link>
+        </section>
+        <nav className={styles.nav} >
+          <ul>
+            <li>
+              <Link to="/">Sitckers</Link>
+            </li>
+            <li>
+              <Link to="/">Sitckers</Link>
+            </li>
+            <li>
+              <Link to="/">Sitckers</Link>
+            </li>
+          </ul>
+        </nav>
       </section>
-      <nav className={styles.nav} >
-        <ul>
-          <li>
-            <Link to="/">Sitckers</Link>
-          </li>
-          <li>
-            <Link to="/">Sitckers</Link>
-          </li>
-          <li>
-            <Link to="/">Sitckers</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   )
 }
